1ca10afc5b66c0d80189354742b7a842fbbc1048
Santu506 <140848902+Santu506@users.noreply.github.com>
2025-05-05 23:59:15 +0530

    change footer logo

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
       <div>
-        <img className="mb-5 w-32" src={assets.logo} alt="" />
+         <img className="mb-5 w-72 rounded-md" src={assets.logo2} alt="" />
         <p className="w-full md:w-2/3 text-gray-600">
           Thank you for visiting our site! We're committed to providing you with
           exceptional service and valuable information. For any inquiries or
